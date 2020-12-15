3655cd38f4c85afe2b574c965c7e9c3d487b3e43
Lucas Torri <lucastorri@gmail.com>
2020-12-15 11:29:51 +0100

    Organize renderers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import createParser, { Table, Cardinality } from "./parser";
-import { readFileSync } from "fs";
+import createParser, { Table, Cardinality, Group, DBML, Ref } from "./parser";
+import { existsSync, readFileSync } from "fs";
 import vizRenderStringSync from "@aduh95/viz.js/sync";
-import yargs from "yargs";
+import yargs, { number } from "yargs";
 import fs from "fs";
 
 type Formats = "dot" | "svg";
@@ -48,35 +48,39 @@ const parse = createParser(
   readFileSync(__dirname + "/../src/dbml.pegjs", "utf-8")
 );
 
-const dbml = parse(args.input);
+type RowAttributes = {
+  [key: string]: string;
+};
 
-const columnRows: Map<string, string> = new Map();
-const tables: Map<string, { table: Table; dot: string }> = new Map();
+class RowRenderer {
+  readonly port: string;
+  private label: string;
+  private attributes: string;
 
-dbml.tables.forEach((table) => {
-  const alias = table.alias || table.name;
+  constructor(port: number, label: string, attributes: RowAttributes) {
+    this.port = `f${port}`;
+    this.label = label;
+    this.attributes = Object.entries(attributes)
+      .map(([key, value]) => `${key}="${value}"`)
+      .join(" ");
+  }
+  toDot(): string {
+    return `<TR><TD PORT="${this.port}" ${this.attributes}>${this.label}</TD></TR>`;
+  }
+}
 
-  const rows = table.columns
-    .map((column, i) => {
-      const port = i + 1;
-      const ref = `${alias}:f${port}`;
-      columnRows.set(`${alias}.${column.name}`, ref);
-      columnRows.set(`${table.name}.${column.name}`, ref);
+class TableRenderer {
+  private table: Table;
+  private columns: Map<string, RowRenderer> = new Map();
 
-      if ("ref" in column.settings) {
-        const ref: string = column.settings["ref"];
-        const [_, cardinality, toTable, toColumn] = ref.split(
-          /([-<>])\s+([\w_]+)\.([\w_]+)/
-        );
-        dbml.refs.push({
-          cardinality: cardinality as Cardinality,
-          fromTable: table.name,
-          fromColumns: [column.name],
-          toTable,
-          toColumns: [toColumn],
-        });
-      }
+  constructor(table: Table) {
+    this.table = table;
+    this.addRow("__TABLE_NAME__", `<B>       ${table.name}       </B>`, {
+      WIDTH: "150",
+      BGCOLOR: "#cccccc",
+    });
 
+    table.columns.forEach((column) => {
       const nameTransformations: ((name: string) => string)[] = [];
       if ("pk" in column.settings) {
         nameTransformations.push((name: string) => `<b>${name}</b>`);
@@ -90,54 +94,136 @@ dbml.tables.forEach((table) => {
         (n) => n
       );
 
-      return `<TR><TD PORT="f${port}">${transformName(column.name)}</TD></TR>`;
-    })
-    .join("|");
+      this.addRow(column.name, transformName(column.name));
+    });
+  }
 
-  const value = {
-    table,
-    dot: `${alias} [label=<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="10" >
-      <TR><TD PORT="f0" WIDTH="150" bgcolor="#cccccc"><B>       ${table.name}       </B></TD></TR>
-      ${rows}
-    </TABLE>>];`,
-  };
+  private addRow(
+    name: string,
+    label: string,
+    attributes?: RowAttributes
+  ): void {
+    this.columns.set(
+      name,
+      new RowRenderer(this.columns.size, label, attributes || {})
+    );
+  }
 
-  tables.set(alias, value);
-  tables.set(table.name, value);
-});
+  ref(column: string): string {
+    if (!this.columns.has(column)) {
+      throw new Error(`Unknown column ${this.table.name}.${column}`);
+    }
+    return `${this.table.name}:${this.columns.get(column)!.port}`;
+  }
 
-const groups = dbml.groups
-  .map((group) => {
-    const tablesInGroup = group.tables
-      .map((tableName) => {
-        const table = tables.get(tableName);
-        if (!table) {
-          throw new Error(
-            `Could not find table ${tableName} for group ${group.name}`
-          );
-        }
+  refAll(columns: string[]): string {
+    //TODO: check that all columns exist
+    //TODO: check that columns together are pk
+    const key = columns.sort().join(",");
+    if (!this.columns.has(key)) {
+      this.addRow(key, `<font color="grey"><i>${key}</i></font>`);
+    }
+    return this.ref(key);
+  }
 
-        tables.delete(table.table.name);
-        if (table.table.alias) tables.delete(table.table.alias);
+  toDot(): string {
+    return `"${
+      this.table.name
+    }" [label=<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="10" >
+      ${Array.from(this.columns.values())
+        .map((column) => column.toDot())
+        .join("\n")}
+    </TABLE>>];`;
+  }
+}
 
-        return table.dot;
-      })
-      .join("\n");
+class TablesRenderer {
+  private renderers: Map<string, TableRenderer> = new Map();
 
-    return `subgraph cluster_${group.name} {
+  constructor(tables: Table[]) {
+    tables.forEach((table) => {
+      const renderer = new TableRenderer(table);
+      this.renderers.set(table.name, renderer);
+      table.alias && this.renderers.set(table.alias, renderer);
+    });
+  }
+
+  get(table: string): TableRenderer {
+    const tableRenderer = this.renderers.get(table);
+    if (!tableRenderer) {
+      throw new Error(`Unknown table ${table}`);
+    }
+    return tableRenderer;
+  }
+
+  names(): Set<string> {
+    return new Set(this.renderers.keys());
+  }
+}
+
+class GroupRenderer {
+  readonly name: string;
+  private tables: TableRenderer[];
+  constructor(group: Group, tables: TableRenderer[]) {
+    this.name = group.name;
+    this.tables = tables;
+  }
+
+  toDot(): string {
+    return `subgraph cluster_${this.name} {
+      rankdir=LR;
       style=filled;
       color="#dddddd";
-      label = "${group.name}"
+      label = "${this.name}"
       margin=20
 
-      ${tablesInGroup}
-  }`;
-  })
-  .join("\n");
+      ${this.tables.map((table) => table.toDot()).join("\n")}
+    }`;
+  }
+}
 
-const remainingTables = Array.from(tables.values())
-  .map((v) => v.dot)
-  .join("\n");
+class UngroupedRenderer {
+  private tables: TableRenderer[];
+  constructor(tables: TableRenderer[]) {
+    this.tables = tables;
+  }
+  toDot(): string {
+    return this.tables.map((table) => table.toDot()).join("\n");
+  }
+}
+
+class GroupsRenderer {
+  private groups: GroupRenderer[];
+  private ungrouped: UngroupedRenderer;
+
+  constructor(groups: Group[], tables: TablesRenderer) {
+    const remainingTables = tables.names();
+    this.groups = groups.map((group) => {
+      return new GroupRenderer(
+        group,
+        group.tables.map((name) => {
+          if (!remainingTables.delete(name)) {
+            throw new Error(
+              `Table ${name} does not exist or belongs to two groups`
+            );
+          }
+          return tables.get(name);
+        })
+      );
+    });
+
+    this.ungrouped = new UngroupedRenderer(
+      Array.from(remainingTables).map((table) => tables.get(table))
+    );
+  }
+
+  toDot(): string {
+    return `
+      ${this.groups.map((group) => group.toDot()).join("\n")}
+      ${this.ungrouped.toDot()}
+    `;
+  }
+}
 
 const refLabels = {
   ">": ["*", "1"],
@@ -145,34 +231,81 @@ const refLabels = {
   "-": ["1", "1"],
 };
 
-const refs = dbml.refs
-  .map((ref) => {
-    const fromKey = `${ref.fromTable}.${ref.fromColumns[0]}`;
-    const fromRow = columnRows.get(fromKey);
-    if (!fromRow) throw new Error(`Could not find ${fromKey} used in from ref`);
+class RefRenderer {
+  private ref: Ref;
+  private fromRef: string;
+  private toRef: string;
+  constructor(ref: Ref, tables: TablesRenderer) {
+    this.ref = ref;
 
-    const toKey = `${ref.toTable}.${ref.toColumns[0]}`;
-    const toRow = columnRows.get(toKey);
-    if (!toRow) throw new Error(`Could not find ${toKey} used in to ref`);
+    this.fromRef = this.findRef(tables.get(ref.fromTable), ref.fromColumns);
+    this.toRef = this.findRef(tables.get(ref.toTable), ref.toColumns);
+  }
+  private findRef(table: TableRenderer, columns: string[]): string {
+    if (columns.length === 1) {
+      return table.ref(columns[0]);
+    } else {
+      return table.refAll(columns);
+    }
+  }
 
-    const [tailLabel, headLabel] = refLabels[ref.cardinality];
+  toDot(): string {
+    const [tailLabel, headLabel] = refLabels[this.ref.cardinality];
+    return `${this.fromRef}:e -> ${this.toRef}:w [headlabel="${headLabel}", taillabel="${tailLabel}", arrowhead="normal", arrowtail="none"]`;
+  }
+}
 
-    return `${fromRow}->${toRow} [headlabel="${headLabel}",taillabel="${tailLabel}", arrowhead="none", arrowtail="none"]`;
-  })
-  .join("\n");
+class DbmlRenderer {
+  private groups: GroupsRenderer;
+  private refs: RefRenderer[];
+  constructor(dbml: DBML) {
+    const tables = new TablesRenderer(dbml.tables);
 
-const dot = `digraph dbml {
-  graph [pad="0", ranksep="0", nodesep="4"]
-  node [shape=none, style=filled, fillcolor=aliceblue, margin=0, fontname=arial, fontsize=18];
-  fontname=arial;
-  nodesep=5.0;
+    this.groups = new GroupsRenderer(dbml.groups, tables);
 
-  ${groups}
-  ${remainingTables}
-  ${refs}
-}`;
+    this.refs = dbml.refs
+      .concat(this.findRefsInSettings(dbml.tables))
+      .map((ref) => new RefRenderer(ref, tables));
+  }
 
-const svg = vizRenderStringSync(dot, {
+  private findRefsInSettings(tables: Table[]): Ref[] {
+    const extraRefs: Ref[] = [];
+    tables.forEach((table) => {
+      table.columns.forEach((column) => {
+        if ("ref" in column.settings) {
+          const ref: string = column.settings["ref"];
+          const [_, cardinality, toTable, toColumn] = ref.split(
+            /([-<>])\s+([\w_]+)\.([\w_]+)/
+          );
+          extraRefs.push({
+            cardinality: cardinality as Cardinality,
+            fromTable: table.name,
+            fromColumns: [column.name],
+            toTable,
+            toColumns: [toColumn],
+          });
+        }
+      });
+    });
+    return extraRefs;
+  }
+
+  toDot(): string {
+    return `digraph dbml {
+      graph [pad="0", ranksep="0", nodesep="4"]
+      node [shape=none, style=filled, fillcolor=aliceblue, margin=0, fontname=arial, fontsize=18];
+      fontname=arial;
+      nodesep=5.0;
+
+      ${this.groups.toDot()}
+      ${this.refs.map((ref) => ref.toDot()).join("\n")}
+    }`;
+  }
+}
+
+const dbml = new DbmlRenderer(parse(args.input));
+
+const svg = vizRenderStringSync(dbml.toDot(), {
   engine: "dot",
   format: args.format,
 });
