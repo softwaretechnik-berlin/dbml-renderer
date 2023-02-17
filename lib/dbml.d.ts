15ab7d8ed53249cefd6e76ee5162ee8de5ee4578
Lucas Torri <lucas.torri@softwaretechnik.berlin>
2023-02-17 16:28:28 +0100

    Add action to publish to npm

--- lib/dbml.d.ts
+++ lib/dbml.d.ts
@@ -1,11 +1,7 @@
-declare function peg$SyntaxError(message: any, expected: any, found: any, location: any): void;
+declare function peg$SyntaxError(message: any, expected: any, found: any, location: any): Error;
 declare class peg$SyntaxError {
     constructor(message: any, expected: any, found: any, location: any);
-    message: any;
-    expected: any;
-    found: any;
-    location: any;
-    name: string;
+    format(sources: any): string;
 }
 declare namespace peg$SyntaxError {
     function buildMessage(expected: any, found: any): string;
